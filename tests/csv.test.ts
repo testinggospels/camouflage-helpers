25fbd19551b8ddd8efc7ba5cdf5a3bcfe3170a95
Shubhendu Madhukar <shubhendumadhukar@gmail.com>
2024-05-26 16:32:14 +0530

    fix: csv test

--- tests/csv.test.ts
+++ tests/csv.test.ts
@@ -8,11 +8,25 @@ let template: HandlebarsTemplateDelegate<any> | undefined;
 
 beforeAll(() => {
     csvcamoflageHelper()
-    template = Handlebars.compile(`{{#each (csv src='${CSV_FILE_PATH}' delimiter=';' key='LatD' value='43')}}{{#with this}}{{State}}:{{City}}{{/with}}{{/each}}`)
+    // template = Handlebars.compile(`{{#each (csv src='${CSV_FILE_PATH}' delimiter=';' key='LatD' value='43')}}{{#with this}}{{State}}:{{City}}{{/with}}{{/each}}`)
+    template = Handlebars.compile(`{{csv src='${CSV_FILE_PATH}' delimiter=';' key='LatD' value='43'}}`)
 })
 test("csv helper - fetch a city/state using LatD", () => {
     if (template) {
-        const cityState = template({})
-        expect(cityState).toBe("WI:Wisconsin Dells")
+        const cityState = JSON.parse(decodeHTMLEntities(template({})))
+        expect(cityState[0].City).toBe("Wisconsin Dells")
     }
 })
+
+function decodeHTMLEntities(text: string) {
+    const entities: Record<string, string> = {
+        '&quot;': '"',
+        '&apos;': "'",
+        '&lt;': '<',
+        '&gt;': '>',
+        '&amp;': '&'
+        // Add more entities if needed
+    };
+
+    return text.replace(/&quot;|&apos;|&lt;|&gt;|&amp;/g, match => entities[match]);
+}
